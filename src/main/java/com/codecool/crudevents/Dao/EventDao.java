bc60a3179d00559586ee82d5145235368cb642a7
Łukasz <l.bielenin@gmail.com>
2017-05-07 20:46:50 +0200

    Create JDBCHandler class with constructor

---
+++ src/main/java/com/codecool/crudevents/Dao/EventDao.java
@@ -0,0 +1,7 @@
+package com.codecool.crudevents.Dao;
+
+/**
+ * Created by mercutio on 07.05.17.
+ */
+public class EventDao {
+}
---
+++ src/main/java/com/codecool/crudevents/Dao/JDBCHandler.java
@@ -0,0 +1,73 @@
+package com.codecool.crudevents.Dao;
+
+import java.sql.*;
+
+/**
+ * Created by mercutio on 07.05.17.
+ */
+public class JDBCHandler {
+
+    private static JDBCHandler ourInstance = new JDBCHandler();
+    private Connection conn = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+
+    private JDBCHandler() {
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/eventcrud.db");
+            statement = conn.createStatement();
+            conn.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Error!");
+        }
+    }
+
+    public static JDBCHandler getInstance() {
+        return ourInstance;
+    }
+
+    public Connection getJDBC() {
+        return conn;
+    }
+
+    public void closeJDBC() {
+        try {
+            clearAndPrepare();
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void executeUpdateQuery(String query) {
+        try {
+            clearAndPrepare();
+            this.statement.executeUpdate(query);
+            this.statement.close();
+            this.conn.commit();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public ResultSet executeSelectQuery(String query) {
+        try {
+            clearAndPrepare();
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return resultSet;
+    }
+
+    private void clearAndPrepare() throws SQLException {
+        if (statement != null) {
+            statement.close();
+        }
+        if (resultSet != null) {
+            resultSet.close();
+        }
+    }
+}
