15fff266351bc41b013a472f54306e6ecc6b1e65
Łukasz <l.bielenin@gmail.com>
2017-05-07 19:08:00 +0200

    Create Event and Descriptions models with constructors

---
+++ src/main/java/com/codecool/crudevents/model/Description.java
@@ -0,0 +1,29 @@
+package com.codecool.crudevents.model;
+
+/**
+ * Created by mercutio on 07.05.17.
+ */
+public class Description {
+    private String basicInfo;
+    private String additionalInfo;
+
+    public Description(String basicInfo) {
+        this.basicInfo = basicInfo;
+    }
+
+    public String getBasicInfo() {
+        return basicInfo;
+    }
+
+    public void setBasicInfo(String basicInfo) {
+        this.basicInfo = basicInfo;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+}
---
+++ src/main/java/com/codecool/crudevents/model/Event.java
@@ -0,0 +1,21 @@
+package com.codecool.crudevents.model;
+
+import java.util.Calendar;
+
+/**
+ * Created by mercutio on 07.05.17.
+ */
+public class Event {
+
+    private String name;
+    private Calendar startDate;
+    private Calendar endDate;
+    private Description description;
+
+    public Event(String name, Calendar startDate, Calendar endDate, Description description) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+}
